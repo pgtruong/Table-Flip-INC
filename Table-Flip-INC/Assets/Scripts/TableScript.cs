55638b1d720064cc8816a59ac79d9fe7d9416f0b
pgtruong <petergtruong@gmail.com>
2017-02-10 14:47:38 -0800

    Tables move and delete, need to flip correctly

---
+++ Table-Flip-INC/Assets/Scripts/TableScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableScript : MonoBehaviour {
+
+    bool flip = false;
+    public GameObject table;
+
+    void Update()
+    {
+        if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || Input.GetMouseButtonDown(0) && !flip)
+        {
+            flip = true;
+            GameObject temp = Instantiate(table);
+            temp.name = "Table";
+        }                    
+        if (flip)
+        {            
+            transform.position = Vector3.Slerp(transform.position, new Vector3(11, -2, 0), Time.deltaTime * 3f);
+            if (!GetComponent<SpriteRenderer>().isVisible)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
